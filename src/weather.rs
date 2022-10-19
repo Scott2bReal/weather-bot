9226264f532790a0d7358689f8e450fc0b53daca
Scott Hoecker <hoeckers@gmail.com>
2022-10-19 06:52:23 -0500

    Type weather api response

--- src/weather.rs
+++ src/weather.rs
@@ -1,4 +1,33 @@
 use serde::Deserialize;
 
 #[derive(Deserialize, Debug)]
-#[serde(rename_all) = "Pascal Case"]
+#[serde(rename_all = "PascalCase")]
+pub struct Location {
+    key: String,
+    localized_name: String,
+    country: Country,
+}
+
+impl Display for Location {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}, {}", self.localized_name, self.country.id)
+    }
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Country {
+    #[serde(alias = "ID")]
+    pub id: String,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "PascalCase")]
+pub struct Forecast {
+    pub headline: Headline,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Headline {
+    #[serde(alias = "Text")]
+    pub overview: String,
+}
