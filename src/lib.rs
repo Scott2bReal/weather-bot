b5fcbd8638d5a0467d1d0a827a089c17eeab567b
Scott Hoecker <hoeckers@gmail.com>
2022-10-19 06:32:23 -0500

    Set up basic response

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,49 @@
 use anyhow::anyhow;
-use serenity::async_trait;
-use serenity::model::channel::Message;
 use serenity::model::gateway::Ready;
+use serenity::model::prelude::interaction::{Interaction, InteractionResponseType};
 use serenity::prelude::*;
+use serenity::{async_trait, model::prelude::GuildId};
 use shuttle_secrets::SecretStore;
-use tracing::{error, info};
+use tracing::info;
 
 struct Bot;
 
 #[async_trait]
 impl EventHandler for Bot {
-    async fn message(&self, ctx: Context, msg: Message) {
-        if msg.content == "!hello" {
-            if let Err(e) = msg.channel_id.say(&ctx.http, "world!").await {
-                error!("Error sending message: {:?}", e);
-            }
-        }
+    async fn ready(&self, ctx: Context, ready: Ready) {
+        info!("{} is connected!", ready.user.name);
+
+        let guild_id = GuildId(628079435832098827);
+
+        let commands = GuildId::set_application_commands(&guild_id, &ctx.http, |commands| {
+            commands.create_application_command(|command| {
+                command.name("hello").description("say hello")
+            })
+        })
+        .await
+        .unwrap();
+
+        info!("{:?}", commands);
     }
 
-    async fn ready(&self, _: Context, ready: Ready) {
-        info!("{} is connected!", ready.user.name);
+    async fn interaction_create(&self, ctx: Context, interaction: Interaction) {
+        if let Interaction::ApplicationCommand(command) = interaction {
+            let response_content = match command.data.name.as_str() {
+                "hello" => "hello".to_owned(),
+                command => unreachable!("Unknown command: {}", command),
+            };
+
+            let create_interaction_response =
+                command.create_interaction_response(&ctx.http, |response| {
+                    response
+                        .kind(InteractionResponseType::ChannelMessageWithSource)
+                        .interaction_response_data(|message| message.content(response_content))
+                });
+
+            if let Err(why) = create_interaction_response.await {
+                eprintln!("Cannot respond to slash command: {}", why);
+            }
+        }
     }
 }
 
